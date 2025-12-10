de03713ca5bce8e584bd0a6a97c5472097fa7fb3
Fitz Koch <fw.j.keenan.koch@gmail.com>
2025-12-10 13:15:39 -0500

    stable

--- src/analysis/viz.py
+++ src/analysis/viz.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -9,6 +8,8 @@ def chart_gender():
     df = clean_process_data()
     gender_count = df.groupby('gender')['count'].sum().reset_index()
     sns.barplot(data=gender_count, x='gender', y='count')
+    plt.title("Livi's Gender Plot")
+    plt.xticks(rotation=45)
     plt.savefig("latex/figures/gender_bar.png")
 
 
